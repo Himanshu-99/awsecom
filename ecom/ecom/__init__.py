67703adde192294e68ac9ac6a6f8e67481a931c3
Himanshu <hanwatehimanshu99@gmail.com>
2024-03-30 02:23:53 +0530

    changes

--- ecom/ecom/__init__.py
+++ ecom/ecom/__init__.py
@@ -1,3 +1,3 @@
 import pymysql
 
-pymysql.install_as_MYSQLdb()
+pymysql.connect(host='ecom29.c3wck2aiu7l5.ap-south-1.rds.amazonaws.com', user='himanshu', password='himanshu1811', database='aws_ecom')
